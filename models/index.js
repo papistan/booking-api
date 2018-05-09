1358c018aaac4c9af2a8e26f9bb4805d82be922a
papistan <jpapisan@gmail.com>
2018-05-08 22:51:43 -0700

    Adds truck and job schema and models. Adds prettier, husky and lint-staged for formatting

---
+++ models/index.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema,
+  model = mongoose.model.bind(mongoose),
+  ObjectId = mongoose.Schema.Types.ObjectId;
+
+const jobSchema = new Schema({
+  name: String,
+  date: Date,
+  startTime: Number,
+  totalHours: Number,
+  truck: { type: ObjectId, ref: "Truck" },
+  created_at: Date
+});
+
+const Job = model("Job", jobSchema);
+
+const truckSchema = new Schema({
+  id: ObjectId,
+  name: String,
+  startTime: Number,
+  endTime: Number,
+  jobs: [{ type: ObjectId, ref: "Job" }],
+  created_at: Date
+});
+
+const Truck = model("Truck", truckSchema);
+
+module.exports = {
+  Truck,
+  Job
+};
